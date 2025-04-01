32c1adaa2b5f5d8c8d73d661f550d20a7464f3f6
Peteroms <omondipeter934@gmail.com>
2025-04-01 09:09:19 +0300

    plot squares

--- m_squares.py
+++ m_squares.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 # pyplot contains a number of functions that help generate charts and plots
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(input_values, squares, linewidth=2) # linewidth Controls the thickness of the line generated
+
+# set chart title and label axes
+plt.title("Square Numbers", fontsize=14)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
 plt.show()   # plt.show() functions opens matplotlib viewer and displays the plot
 
 
