79386d2851a6dbe8b5230440b78b0803bae120a1
Peteroms <omondipeter934@gmail.com>
2025-04-01 08:41:52 +0300

    plot a simple line

---
+++ m_squares.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+# pyplot contains a number of functions that help generate charts and plots
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()   # plt.show() functions opens matplotlib viewer and displays the plot
+
+
+
